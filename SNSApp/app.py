8e24f03c80f15cce3c83d41073d2debc0ff46183
matsukan <five08016161@gmail.com>
2026-01-20 00:02:23 +0900

    feat: 画面のベース部分、ログイン画面、ホーム画面のファイルを作成しました

--- SNSApp/app.py
+++ SNSApp/app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def hello():
-    return 'Hello World'
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/base', methods=['GET'])
+def base():
+    return render_template('base.html')
+
+
+@app.route('/posts', methods=['GET'])
+def posts():
+    return render_template('posts.html')
 
 
 if __name__ == '__main__':
